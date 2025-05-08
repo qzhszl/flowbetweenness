30e046bee0d3cba2a006ca1f91c4bbf8c6b50975
qzh <867483679@qq.com>
2025-05-08 22:22:07 +0200

    202505082222

--- IERP/Ieffectiveresitance.m
+++ IERP/Ieffectiveresitance.m
@@ -23,21 +23,21 @@ clear, clc
 
 % 1. generate a graph
 % (a) tree:
-N = 10;
-% T = generate_a_tree(N,1,10);
-% A = full(adjacency(T,"weighted"));
-A_input = [0	0	0	0	0	0	0	0	3	0
-    0	0	0	0	0	0	6	10	0	0
-    0	0	0	0	2	0	0	2	0	0
-    0	0	0	0	0	0	0	4	0	0
-    0	0	2	0	0	10	0	0	4	0
-    0	0	0	0	10	0	0	0	0	0
-    0	6	0	0	0	0	0	0	0	0
-    0	10	2	4	0	0	0	0	0	0
-    3	0	0	0	4	0	0	0	0	3
-    0	0	0	0	0	0	0	0	3	0];
+N = 100;
+T = generate_a_tree(N,1,10);
+A_input = full(adjacency(T,"weighted"));
+% A_input = [0	0	0	0	0	0	0	0	3	0
+%     0	0	0	0	0	0	6	10	0	0
+%     0	0	0	0	2	0	0	2	0	0
+%     0	0	0	0	0	0	0	4	0	0
+%     0	0	2	0	0	10	0	0	4	0
+%     0	0	0	0	10	0	0	0	0	0
+%     0	6	0	0	0	0	0	0	0	0
+%     0	10	2	4	0	0	0	0	0	0
+%     3	0	0	0	4	0	0	0	0	3
+%     0	0	0	0	0	0	0	0	3	0];
 
-T = graph(A_input);
+% T = graph(A_input);
 % subplot(2,2,1)
 % h1 = plot(T,'EdgeLabel',T.Edges.Weight,'NodeColor',[0.8500 0.3250 0.0980], ...
 % 'EdgeAlpha',0.5,'LineWidth',1,'MarkerSize',7,'EdgeLabelColor',[0 0.4470 0.7410],'NodeFontSize',10);
@@ -54,20 +54,20 @@ Input_Omega = D;
 W_tilde  = Input_Omega
 W_tilde(W_tilde ~= 0) = 1 ./ W_tilde(W_tilde ~= 0);
 Omega_new = EffectiveResistance(W_tilde)            % Compute the effective resistance Omega
-D-Omega_new
+D-Omega_new;
 sum(sum(D-Omega_new))
 val = 1;
 A = (W_tilde > 0);
 while(nnz(Omega_new > D) == 0 && val > 0)                     % Remove links one by one until we exceed the constraints                            
     % method 1 R = A.*((Omega_new+eye(N)).^-1 - W_tilde)*(D-Omega_new) 
-    R = A.*((Omega_new+eye(N)).^-1 - W_tilde).*(D-Omega_new)     % Compute R
+    R = A.*((Omega_new+eye(N)).^-1 - W_tilde).*(D-Omega_new);     % Compute R
     [val,~] = max(max(R));                              % Identify the maximum element
     [row,col] = find(R == val);                        % Identify the link
     A(row(1),col(1)) = 0; A(col(1),row(1)) = 0;        % Remove the link
     W_tilde = A.*D;
     W_tilde(W_tilde ~= 0) = 1 ./ W_tilde(W_tilde ~= 0);      % Compute W tilde
     Omega_new = EffectiveResistance(W_tilde);               % Update the shortest path weight matrix
-    D-Omega_new                             
+    D-Omega_new ;                            
     sum(sum(D-Omega_new))
 end
 
