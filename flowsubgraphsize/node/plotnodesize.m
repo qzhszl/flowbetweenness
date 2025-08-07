a811d9658f7d45468a7601f0e665d742471b7528
qzh <867483679@qq.com>
2025-08-07 22:12:29 +0200

    20250807home

--- flowsubgraphsize/node/plotnodesize.m
+++ flowsubgraphsize/node/plotnodesize.m
@@ -1,10 +1,17 @@
 clear,clc
 % plot the relation between the flow subgraph node size with the real ave degree
 N = 100
+
 avefsg_size_filename = sprintf("D:\\data\\flow betweenness\\sizeofflowsubgraph\\nodesize10000node\\%dnode\\ave_fsg_withdiff_avg.txt",N);
+% avefsg_size_filename = sprintf("D:\\data\\flow betweenness\\sizeofflowsubgraph\\nodesize10000node\\%dnode\\ave_fsg_withdiff_avg.txt",N);
 result_table = readtable(avefsg_size_filename);
 plot(result_table.avg,result_table.Mean/100)
-
+p_vals = result_table.avg/(N-1);
+s_vals = zeros(size(p_vals));
+for i = 1:length(p_vals)
+    s_vals(i) = compute_s_from_ER(N, p_vals(i));
+end
+plot(ave_degree, s_vals, 'LineWidth', 2)
 
 % for large network
 %__________________________________________________________________________
