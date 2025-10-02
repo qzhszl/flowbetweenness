7e47eb6aa9a92277f35641f8e7bd6d926df16689
qzh <867483679@qq.com>
2025-10-02 22:19:21 +0200

    20251002home

---
+++ powerdissipation/compute_power_dissipation_eachlink_BA.m
@@ -0,0 +1,97 @@
+function compute_power_dissipation_eachlink_BA()
+%SIMULATE_ER_POWER 重复生成 ER 图，计算所有节点对的能量消耗
+%
+% 输入:
+%   n          节点数
+%   p          ER 图连边概率
+%   simu_times 重复次数
+%
+% 输出:
+%   Results_SP   每次仿真 shortest-path 模型能量
+%   Results_Flow 每次仿真 current-flow 模型能量
+%
+% 要求：已存在两个函数
+%   [power_givenst_SP, EdgeEnergy_SP] = compute_path_power_dissipation(G_path,s,t)
+%   [power_givenst_flow, EdgeEnergy_flow] = compute_flownetwork_power_dissipation(G,s,t)
+%
+% 其中 G_path 的边权重 = G 的边权重的倒数
+simu_times=1;
+results = struct();
+
+for k = 1:simu_times
+    fprintf('rep %d/%d\n', k, simu_times);
+    % ---- 生成 ER 图 ----
+    % G = BAgraph(1000, 4, 3);
+    n=5000;
+    m=3;
+    networkname = sprintf('BAnetworkN%dm%d.txt',n,m);
+    data = readmatrix(networkname);  % 每行: u v w
+    
+    % 提取三列
+    s = data(:,1)+1;    % 起点
+    t = data(:,2)+1;    % 终点
+    w = data(:,3);    % 权重
+    
+    % 构造无向图（如果是有向图，就用 digraph）
+    G = graph(s, t, w);
+%     A = adjacency(G,"weighted");
+%     G = graph(A);  % 无向图
+%     figure;
+%     plot(G,'EdgeLabel',G.Edges.Weight,'NodeColor',[0.8500 0.3250 0.0980], ...
+% 'EdgeAlpha',0.5,'LineWidth',1,'MarkerSize',7,'EdgeLabelColor',[0 0.4470 0.7410],'NodeFontSize',10);
+
+    % 随机生成边权重 (导纳)，避免 0 权
+    
+    % G_path 的边权重 = 1/g
+    % figure;
+    G_path = graph(G.Edges.EndNodes(:,1), G.Edges.EndNodes(:,2), 1./G.Edges.Weight,numnodes(G));
+%     plot(G_path,'EdgeLabel',G_path.Edges.Weight,'NodeColor',[0.8500 0.3250 0.0980], ...
+% 'EdgeAlpha',0.5,'LineWidth',1,'MarkerSize',7,'EdgeLabelColor',[0 0.4470 0.7410],'NodeFontSize',10);
+
+    % ---- 对所有节点对 (i,j) 计算 ----
+    n = numnodes(G);
+    total_SP   = zeros(nchoosek(n,2),1);   
+    total_Flow = zeros(nchoosek(n,2),1);
+    linkP_SP = zeros(numedges(G),1);
+    linkP_Flow = zeros(numedges(G),1);
+    count = 0;
+    for s = 1:n
+        for t = (s+1):n
+            count = count+1;
+            % shortest path
+            
+            [total_SP_fornodepair, link_SP_fornodepair,connected_flag] = compute_path_power_dissipation(G_path,s,t);
+            
+            if connected_flag==1
+    
+                total_SP(count) = total_SP_fornodepair; 
+                linkP_SP = linkP_SP+link_SP_fornodepair;
+                % current flow
+                [total_Flow_fornodepair, link_Flow_fornodepair] = compute_flownetwork_power_dissipation(G,s,t);
+                total_Flow(count) = total_Flow_fornodepair;
+                linkP_Flow = linkP_Flow+link_Flow_fornodepair;
+
+                [total_Flow_fornodepair2, link_Flow_fornodepair2] = compute_flownetwork_power_dissipation_original(G,s,t);
+                
+
+                find(abs(link_Flow_fornodepair-link_Flow_fornodepair2)>0.000001)
+                
+            end
+        end
+    end
+    
+    % 保存结果
+    results(k).edges = G.Edges;  % [i j w]
+    results(k).total_Flow = total_Flow;
+    results(k).total_SP = total_SP;
+    results(k).linkP_Flow = linkP_Flow;
+    results(k).linkP_SP = linkP_SP;
+end
+
+filename = sprintf('power_dissipation_N%dm%d.mat',n,m);
+% 保存到文件
+save(filename,'results');
+disp("mission_completed")
+end
+
+
